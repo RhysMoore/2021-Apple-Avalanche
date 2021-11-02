db58deeac25d2f8242ad154f6eae74dba804e665
Rhys Moore <Rhys.moore@vtsd.com>
2021-11-02 12:26:14 -0400

    commit

--- main.py
+++ main.py
@@ -3,33 +3,44 @@ import turtle as trtl
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
-
-xcor = 0
-ycor = 0
-
+x_offset = -20
+y_offset = -47
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
 wn.bgpic("background.gif")
 apple = trtl.Turtle()
+wn.tracer(False)
+
+
+
+
+
+
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
+  active_apple.penup()
   active_apple.shape(apple_image)
+  draw_an_A("A", active_apple)
   wn.update()
 
-def draw_an_A(active_apple):
-  active_apple.penup()
-  active_apple.goto(xcor-20, ycor)
+def draw_an_A(letter, active_apple):
   active_apple.color("white")
-  active_apple.write("A", font=("Arial", 55, "bold"))
+  remember_pos = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + x_offset, active_apple.ycor() + y_offset)
+  active_apple.write(letter, font=("Arial", 50, "bold"))
+  active_apple.setpos(remember_pos)
 
-def drop_apple(active_apple):
+def drop_apple():
+  wn.tracer(True)
   apple.penup()
-  apple.goto(xcor, ycor-160)
+  apple.goto(apple.xcor(), -250)
+  wn.tracer(False)
 
 #-----function calls-----
 draw_apple(apple)
-draw_an_A(drop_apple, "a")
+wn.onkeypress(drop_apple, "a")
+wn.listen()
 wn.mainloop()
